1c60b51606291d5ea005c7fe517fd92198ecdc80
Evans <ivanz.corbie@gmail.com>
2024-08-01 00:11:58 +0000

    printf_reversed.c file created

---
+++ printf_reverse_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_reserve_str - prints a string a in reversed order
+* @args: argument list
+* Return: number of characters printed
+* Printf project by Evans Adu and Enoch Kabange
+*/
+
+int print_reserve_str(va_list args)
+{
+char *input_string = va_arg(args, char *);
+int index, count = 0;
+
+if (input_string == NULL)
+input_string = "(null)";
+while (input_string[count])
+count++;
+
+for (index = count - 1; index >= 0; index--)
+_putchar(input_string[index]);
+
+return (count);
+}
