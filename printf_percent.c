5ff8729232600f3f999a9979d84b994520df86db
enochkabange <68244336+enochkabange@users.noreply.github.com>
2024-08-01 02:25:08 +0000

    Add files via upload
    printf_percent.c created

---
+++ printf_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_percent - prints the percentage symbol
+ * Return: integer
+ */
+
+int print_percent(va_list args)
+{
+    (void)args;
+    _putchar('%');
+    return (1);
+}
