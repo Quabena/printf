b654e3842e38531d15875aa930269e0859f07751
Evans <ivanz.corbie@gmail.com>
2024-08-02 19:53:15 +0000

    _putchar.c file created

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,26 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - Prints the character 'c' to stdout
+* @c: The character to print
+* Return: On success 1, or -1 on error
+* Evans Adu and Enoch Kabange
+*/
+
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+static char buffer[1024];
+static int a;
+
+if (a >= 1024 || c == -1)
+{
+write(1, &buffer, a);
+a = 0;
+}
+if (c != -1)
+{
+buffer[a] = c;
+a++;
+}
+return (1);
 }
