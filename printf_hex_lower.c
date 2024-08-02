c9353caf058de97901a685b5859cd1f31b2141dd
Evans <ivanz.corbie@gmail.com>
2024-08-02 20:56:12 +0000

    printf_hex_lower.c file created

---
+++ printf_hex_lower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_hex_lower - Outputs an unsigned integer as a
+* lowercase hexadecimal string.
+* @args: Variable argument list containing the integer to convert
+* @format: Structure containing formatting flags (used for hash flag)
+* Return: The number of characters printed
+* Authors: Evans Adu and Enoch Kabange
+*/
+
+int print_hex_lower(va_list args, flag_settings *format)
+{
+unsigned long int number = va_arg(args, unsigned long int);
+int length = 0;
+
+char *hex_str = convert_to_base(number, 16, 1);
+
+if (format->hash == 1 && hex_str[0] != '0')
+length += _puts("0x");
+length += _puts(hex_str);
+return (length);
+}
